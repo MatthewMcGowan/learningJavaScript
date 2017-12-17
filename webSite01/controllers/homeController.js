8318fe599e5f57e88b52233315b9a8759372c5bb
Matthew McGowan <MatthewMcGowan@users.noreply.github.com>
2017-12-17 20:34:52 +0000

    Used EJS and Vash as view engines.

---
+++ webSite01/controllers/homeController.js
@@ -0,0 +1,9 @@
+(function (homeController) {
+
+    homeController.init = function (app) {
+
+        
+
+    }
+
+})(module.exports);
--- webSite01/expressServer01.js
+++ webSite01/expressServer01.js
@@ -1,13 +1,23 @@
 var http = require("http");
 var express = require("express");
 var app = express();
-// Setup view engine
-app.set("view engine", "jade");
 
+// Setup jade view engine
+//app.set("view engine", "jade");
+
+// Setup EJS view engine
+//var ejsEngine = require("ejs-locals");
+//app.engine("ejs", ejsEngine); // support master pages
+//app.set("view engine", "ejs"); // actually set the view engine
+
+// Setup Vash view engine
+app.set("view engine", "vash");
 
 app.get("/", function (req, res) {
     //res.send("<html><body><h1>Express</h1></body></html>");
-    res.render("jade/index", { title: "Express + Jade" });
+    //res.render("jade/index", { title: "Express + Jade" });
+    //res.render("ejs/index", { title: "Express + EJS" });
+    res.render("vash/index", { title: "Express + Vash" });
 });
 
 app.get("/api/users", function (req, res) {
